801962a4e520b9ecafba94c3866682da6571e9c8
LOW JIA EN <21024625@myrp.edu.sg>
2022-05-27 07:57:17 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21024625/demodatapassingtest/MainActivity.java
@@ -0,0 +1,57 @@
+package sg.edu.rp.c346.id21024625.demodatapassingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Event handling Step 1
+    Button btnPassInteger;
+    Button btnPassChar;
+    TextView textPassDouble;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Event handling Step 2
+        btnPassInteger = findViewById(R.id.buttonPassInteger);
+        btnPassChar = findViewById(R.id.buttonPassChar);
+        textPassDouble = findViewById(R.id.textViewDouble);
+
+        //Event handling Step 3
+        btnPassInteger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("value", 1);
+                startActivity(intent);
+            }
+        });
+
+        btnPassChar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                intent.putExtra("value", 'a');
+                startActivity(intent);
+            }
+        });
+
+        textPassDouble.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FourthActivity.class);
+                intent.putExtra("value", 99.99);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id21024625/demodatapassingtest/ThirdActivity.java
@@ -0,0 +1,26 @@
+package sg.edu.rp.c346.id21024625.demodatapassingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    //Step 1
+    TextView tvAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        //Step 2
+        tvAnswer = findViewById(R.id.textView);
+        Intent intentReceived = getIntent();
+        char value = intentReceived.getCharExtra("value", 'a');
+        tvAnswer.setText("Character value received is: " + value);
+
+    }
+}
